57892eeb84496f75f0fb64d4126bdf3cd5732bc7
JacintaNgwu <jacintangwu9900@gmail.com>
2022-12-22 00:02:21 +0530

    Created game with api

---
+++ src/modules/data.js
@@ -0,0 +1,16 @@
+// create new game
+const createGame = async () => {
+  const gameName = { name: 'My lovely new game' };
+  const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+  const game = await fetch(baseURL, {
+    method: 'post',
+    body: JSON.stringify(gameName),
+    headers: {
+      'content-Type': 'application/json',
+    },
+  });
+  await game.json();
+};
+
+export default createGame;
+// 4SOOJo8RgF1RNSGwY1XP
