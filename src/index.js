e41fdda5db64d3a616eb6fb97bc40757ab6c9d63
JacintaNgwu <jacintangwu9900@gmail.com>
2022-12-22 00:04:35 +0530

    Get and save scores using api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './styles/style.css';
+import ScoreBoard from './modules/loader.js';
+import form from './modules/add.js';
+import refreshBtn from './modules/refresh.js';
+
+ScoreBoard();
+form();
+refreshBtn();
